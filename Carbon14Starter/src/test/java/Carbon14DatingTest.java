1b8be73504112af32f7201429ea911c824e4ee47
cshel482 <csheldon482@gmail.com>
2023-07-18 14:24:30 -0500

    first commit

---
+++ Carbon14Starter/src/test/java/Carbon14DatingTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Carbon14DatingTest {
+    //#1 Build a test for calculateAge();
+
+
+    /*
+        #2 Build a test for setRemainingAmount() in the
+        scenario the given remaining value is <= 1.
+    */
+
+   /*
+        #3 Build a test for getRemainingAmount() in the
+        scenario the given remaining value is >= 1.
+    */
+
+    // #4 Build a test for the toString() method.
+    @Test
+    public void testToString(){
+        Carbon14Dating e = new Carbon14Dating(12);
+        String actual = e.toString();
+        String expected = "The age of the sample is: 17,527.46 years";
+        assertEquals(expected, actual);
+    }
+}
